e4172660f52645aa6911cf89dfff0d90fc39d9c4
Elvin Fatalizade <elvinff@code.edu.az>
2019-04-29 00:36:32 +0400

    Complate Why Choose Eduglobal andMost Popular Categories

--- js/script.js
+++ js/script.js
@@ -2,4 +2,32 @@ $(document).ready(function(){
     $('.carousel').carousel({
         interval: 3000
       })
+      $('.sliders').owlCarousel({
+        loop:true,
+        dosts:false,
+        autoplay:false,
+        margin:5,
+        freeDrag:true,
+        
+       autoplay:false,
+        mouseDrag:true,
+      
+       
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true,
+            },
+            600:{
+                items:3,
+                nav:true,
+            },
+            1000:{
+                items:5,
+                nav:false,
+                loop:false
+            }
+        }
+    })
 });
