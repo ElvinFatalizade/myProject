8b3c82257b56f8e899c4f6101e263dde99d96d21
Elvin Fatalizade <elvinff@code.edu.az>
2019-05-02 23:50:24 +0400

    Complate Our gallery & Our teacher

--- js/script.js
+++ js/script.js
@@ -11,8 +11,6 @@ $(document).ready(function(){
         
        autoplay:false,
         mouseDrag:true,
-      
-       
         responsiveClass:true,
         responsive:{
             0:{
@@ -30,4 +28,36 @@ $(document).ready(function(){
             }
         }
     })
+    // Ours Gallery
+    $('.portfolio-item').isotope({
+        itemSelector: '.item',
+        layoutMode: 'fitRows'
+      });
+      $('.portfolio-menu ul li').click(function(){
+        $('.portfolio-menu ul li').removeClass('active');
+        $(this).addClass('active');
+
+        var selector=$(this).attr('data-filter');
+        $('.portfolio-item').isotope({
+            filter: selector
+        });
+        return false;
+      });
+    //   End Ours Gallery
+
+
+      var cardImg = $("#our-teachers .card-img-top")
+      var links = $("#our-teachers .card-img-top .social")
+      for (let i = 0; i < cardImg.length; i++) {
+          $(cardImg[i]).mouseover(function () {
+              $(links[i]).stop();
+              $(links[i]).animate({ top: "120px" });
+          })
+  
+          $(cardImg[i]).mouseout(function () {
+              $(links[i]).stop();
+              $(links[i]).animate({ top: "255px" });
+          })
+      }
+     
 });
